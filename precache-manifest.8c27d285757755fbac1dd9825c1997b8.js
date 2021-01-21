5a80767e18a79c9d2bdf155f22667c2600e578d1
Viral <patel.viral@northeastern.edu>
2021-01-21 10:41:50 -0500

    Added files again from master

---
+++ precache-manifest.8c27d285757755fbac1dd9825c1997b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a077b35b2ff9541a039eacead12c05a6",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5eae112d56aae7fd0222",
+    "url": "/home/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "883a57c979cc17cde2f2",
+    "url": "/home/static/css/main.5edc7faf.chunk.css"
+  },
+  {
+    "revision": "5eae112d56aae7fd0222",
+    "url": "/home/static/js/2.bfd85c6b.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/home/static/js/2.bfd85c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883a57c979cc17cde2f2",
+    "url": "/home/static/js/main.2e2972a4.chunk.js"
+  },
+  {
+    "revision": "82b407c844beaaec709c",
+    "url": "/home/static/js/runtime-main.1e703b0d.js"
+  },
+  {
+    "revision": "4777d97b89eadfbe31d865e2ad39d533",
+    "url": "/home/static/media/home-backgroung-image_keyboard-mouse.4777d97b.jpeg"
+  },
+  {
+    "revision": "566b1f668141b006e91c407921339d7f",
+    "url": "/home/static/media/khoury_college.566b1f66.png"
+  },
+  {
+    "revision": "e9509129e5b69eb0afed5a1849a2e81e",
+    "url": "/home/static/media/vp-profile-image.e9509129.jpg"
+  }
+]);
