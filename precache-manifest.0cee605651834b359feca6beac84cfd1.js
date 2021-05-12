9a231b7001507c6c41bec9345aef740bb167c1a0
Viral <patel.viral@northeastern.edu>
2021-05-12 18:42:26 -0400

    Copy after reset

---
+++ precache-manifest.0cee605651834b359feca6beac84cfd1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb7e563aa928915897bf540a850f6b9",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "9c39732d1a5b9dd65b31",
+    "url": "/home/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "e3a6969c804d1ba15a85",
+    "url": "/home/static/css/main.18a203cb.chunk.css"
+  },
+  {
+    "revision": "9c39732d1a5b9dd65b31",
+    "url": "/home/static/js/2.65300ad7.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/home/static/js/2.65300ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a6969c804d1ba15a85",
+    "url": "/home/static/js/main.253d6429.chunk.js"
+  },
+  {
+    "revision": "82b407c844beaaec709c",
+    "url": "/home/static/js/runtime-main.1e703b0d.js"
+  },
+  {
+    "revision": "4777d97b89eadfbe31d865e2ad39d533",
+    "url": "/home/static/media/home-backgroung-image_keyboard-mouse.4777d97b.jpeg"
+  },
+  {
+    "revision": "566b1f668141b006e91c407921339d7f",
+    "url": "/home/static/media/khoury_college.566b1f66.png"
+  },
+  {
+    "revision": "5bfd9e82a3dcd74e4a39da05b824cff7",
+    "url": "/home/static/media/northeastern-university.5bfd9e82.png"
+  },
+  {
+    "revision": "116cdba4ee044f8c100b3b9be063418b",
+    "url": "/home/static/media/northeastern_university.116cdba4.png"
+  },
+  {
+    "revision": "a8c01ddea32049d45986267a67eaa11c",
+    "url": "/home/static/media/sbmp.a8c01dde.jpg"
+  },
+  {
+    "revision": "d1a78952a590d1aff1ed6b2cfc451895",
+    "url": "/home/static/media/university-of-mumbai.d1a78952.png"
+  },
+  {
+    "revision": "e9509129e5b69eb0afed5a1849a2e81e",
+    "url": "/home/static/media/vp-profile-image.e9509129.jpg"
+  }
+]);
