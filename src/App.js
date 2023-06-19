02bfd73127a8abd9c369b317d90f30ff0d00dac6
patelviralb <patel.viral@northeastern.edu>
2023-06-19 13:50:30 -0500

    After resetting the app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Good things are on the way....</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
